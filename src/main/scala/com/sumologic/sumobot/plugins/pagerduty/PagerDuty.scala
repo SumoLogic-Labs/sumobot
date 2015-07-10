af6acb65b3dedf83648b10f88c176b0375bee86b
Stefan Zier <stefan@sumologic.com>
2015-07-09 22:27:30 -0700

    Enhancements to the pagerduty plugin.

--- src/main/scala/com/sumologic/sumobot/plugins/pagerduty/PagerDuty.scala
+++ src/main/scala/com/sumologic/sumobot/plugins/pagerduty/PagerDuty.scala
@@ -7,10 +7,15 @@ import com.sumologic.sumobot.plugins.BotPlugin
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
+trait EscalationPolicyFilter {
+  def filter(request: BotMessage, policies: Seq[PagerDutyEscalationPolicy]): Seq[PagerDutyEscalationPolicy]
+}
+
 /**
  * @author Chris (chris@sumologic.com)
  */
-class PagerDuty(manager: PagerDutySchedulesManager) extends BotPlugin with ActorLogging {
+class PagerDuty(manager: PagerDutySchedulesManager,
+                policyFilter: Option[EscalationPolicyFilter] = None) extends BotPlugin with ActorLogging {
 
   override protected def name: String = "pagerduty"
 
@@ -31,7 +36,9 @@ class PagerDuty(manager: PagerDutySchedulesManager) extends BotPlugin with Actor
     case WhosOnCall(filter) => respondInFuture(whoIsOnCall(_, maximumLevel, Option(filter)))
   }
 
-  private[this] def whoIsOnCall(msg: BotMessage, maximumLevel: Int, filterOpt: Option[String]): SendSlackMessage = {
+  private[this] def whoIsOnCall(msg: BotMessage,
+                                maximumLevel: Int,
+                                filterOpt: Option[String]): SendSlackMessage = {
     manager.getEscalationPolicies match {
       case Some(policies) =>
         val escalationPolicies = policies.escalation_policies
@@ -40,8 +47,16 @@ class PagerDuty(manager: PagerDutySchedulesManager) extends BotPlugin with Actor
         }
 
         // TODO: Teach the filter to be smarter about how it handles stuff since this text matching is stupidly simple
-        val nonFilteredPolicies = nonTestPolicies.filter {
-          policy => filterOpt.isEmpty || filterOpt.exists(filter => policy.name.toLowerCase.contains(filter.toLowerCase))
+        val partiallyFilteredPolicies = nonTestPolicies.filter {
+          policy =>
+            (filterOpt.isEmpty ||
+                filterOpt.exists(filter => policy.name.toLowerCase.contains(filter.toLowerCase))) &&
+                policy.on_call.nonEmpty
+        }
+
+        val nonFilteredPolicies = policyFilter match {
+          case Some(filter) => filter.filter(msg, partiallyFilteredPolicies)
+          case None => partiallyFilteredPolicies
         }
 
         if (nonFilteredPolicies.isEmpty) {
@@ -49,14 +64,23 @@ class PagerDuty(manager: PagerDutySchedulesManager) extends BotPlugin with Actor
         } else {
           val outputString = nonFilteredPolicies.map {
             policy =>
-              val onCalls = policy.on_call.filter(_.level <= maximumLevel).map {
-                onCall => s"- level ${onCall.level}: ${onCall.user.name} (${onCall.user.email})"
+              val onCalls = policy.on_call.filter(_.level <= maximumLevel).groupBy(_.level).toList.sortBy(_._1).map {
+                tpl =>
+                  val level = tpl._1
+                  val oncalls = tpl._2.map(_.user.name).sorted.mkString(", ")
+                  val levelName = level match {
+                    case 1 => "primary"
+                    case 2 => "secondary"
+                    case 3 => "tertiary"
+                    case other => s"level $other"
+                  }
+                  s"- _$levelName:_ $oncalls"
               }.mkString("\n", "\n", "\n")
 
-              policy.name + onCalls
+              "*" + policy.name + "*" + onCalls
           }.mkString("\n")
 
-          msg.response(outputString)
+          msg.message(outputString)
         }
 
       case None =>
