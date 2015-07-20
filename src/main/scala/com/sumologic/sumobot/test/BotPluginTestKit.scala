ae83b383b6990dc622c7e430f92ec73da71281a4
Stefan Zier <stefan@sumologic.com>
2015-07-20 16:22:33 -0700

    Adding a unit test framework and initial test.

---
+++ src/main/scala/com/sumologic/sumobot/test/BotPluginTestKit.scala
@@ -0,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.sumologic.sumobot.test
+
+import akka.actor.ActorSystem
+import akka.testkit.{TestProbe, TestKit}
+import com.sumologic.sumobot.core.OutgoingMessage
+import slack.models.User
+import scala.concurrent.duration._
+
+import scala.concurrent.duration.FiniteDuration
+
+class BotPluginTestKit(_system: ActorSystem) extends TestKit(_system) {
+
+  protected val outgoingMessageProbe = TestProbe()
+  system.eventStream.subscribe(outgoingMessageProbe.ref, classOf[OutgoingMessage])
+
+  protected def confirmOutgoingMessage(test: OutgoingMessage => Unit, timeout: FiniteDuration = 1.second): Unit = {
+    outgoingMessageProbe.expectMsgClass(1.second, classOf[OutgoingMessage]) match {
+      case msg: OutgoingMessage =>
+        test(msg)
+    }
+  }
+
+
+
+  protected def mockUser(id: String, name: String): User = {
+    User(id, name, None, None, None, None, None, None, None, None, None, None)
+  }
+}
---
+++ src/test/scala/com/sumologic/sumobot/plugins/help/HelpTest.scala
@@ -0,0 +1,69 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.sumologic.sumobot.plugins.help
+
+import akka.actor.{ActorSystem, Props}
+import com.sumologic.sumobot.core.{IncomingMessage, InstantMessageChannel, PluginRegistry}
+import com.sumologic.sumobot.plugins.BotPlugin.{InitializePlugin, PluginAdded}
+import com.sumologic.sumobot.plugins.conversations.Conversations
+import com.sumologic.sumobot.test.BotPluginTestKit
+import org.scalatest.{Matchers, WordSpecLike}
+
+class HelpTest(_system: ActorSystem) extends BotPluginTestKit(_system) with WordSpecLike with Matchers {
+
+  def this() = this(ActorSystem("HelpTest"))
+
+  val helpRef = system.actorOf(Props[Help], "help")
+
+  val reg = system.actorOf(Props(classOf[PluginRegistry]))
+  val mock = system.actorOf(Props(classOf[Conversations]), "mock")
+
+  reg ! PluginAdded(mock, "mock help")
+  reg ! PluginAdded(helpRef, "help help")
+
+  helpRef ! InitializePlugin(null, null, reg)
+
+  val user = mockUser("123", "jshmoe")
+
+  "help" should {
+    "return list of plugins" in {
+      helpRef ! IncomingMessage("help", true, InstantMessageChannel("125", user), user)
+      confirmOutgoingMessage {
+        msg =>
+          msg.text should be("help\nmock")
+      }
+    }
+
+    "return help for known plugins" in {
+      helpRef ! IncomingMessage("help mock", true, InstantMessageChannel("125", user), user)
+      confirmOutgoingMessage {
+        msg =>
+          msg.text should include("mock help")
+      }
+    }
+
+    "return an error for unknown commands" in {
+      helpRef ! IncomingMessage("help test", true, InstantMessageChannel("125", user), user)
+      confirmOutgoingMessage {
+        msg =>
+          msg.text should include("Sorry, I don't know")
+      }
+    }
+  }
+}
