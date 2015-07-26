b1cb620858aee9a177377306fdd5f764a77a04a1
Stefan Zier <stefan@sumologic.com>
2015-07-26 15:02:49 -0700

    Upgrading to the newest 0.1.1 version of the slack client.

--- src/main/scala/com/sumologic/sumobot/core/SlackSettings.scala
+++ src/main/scala/com/sumologic/sumobot/core/SlackSettings.scala
@@ -21,18 +21,17 @@ package com.sumologic.sumobot.core
 import akka.actor.ActorSystem
 import com.netflix.config.scala.{DynamicIntProperty, DynamicStringProperty}
 import slack.rtm.SlackRtmClient
-
+import scala.concurrent.duration._
 
 object SlackSettings {
   val ApiToken = DynamicStringProperty("slack.api.token", null)
 
-  // TODO: Wire this up when the 0.1.1 version of slack-scala-client is released.
   val ConnectTimeout = DynamicIntProperty("slack.connect.timeout.seconds", 15)
 
   def connectOrExit(implicit system: ActorSystem): SlackRtmClient = {
     ApiToken() match {
       case Some(token) =>
-        SlackRtmClient(token)
+        SlackRtmClient(token, ConnectTimeout.get.seconds)
       case None =>
         println(s"Please set the slack.api.token environment variable!")
         sys.exit(1)
