aa3cb1c4f90b1c047476d901d9c55d1110438cd0
Greg Oledzki <goledzki@sumologic.com>
2018-12-13 10:28:24 +0100

    Upgrading to 0.2.5

--- src/test/scala/com/sumologic/sumobot/core/ReceptionistTest.scala
+++ src/test/scala/com/sumologic/sumobot/core/ReceptionistTest.scala
@@ -44,7 +44,7 @@ class ReceptionistTest
   private val self = new User("U123", "bender", None, None, None, None, None, None, None, None, None, None, None, None, None, None)
   private val somebodyElse = new User("U124", "dude", None, None, None, None, None, None, None, None, None, None, None, None, None, None)
   private val team = new Team("T123", "testers", "example.com", "example.com", 1, false, null, "no plan")
-  private val channel = new Channel("C123", "slack_test", 1, self.id, Some(false), Some(true), Some(false), None, None, None, None, None, None, None, None, None, None, None)
+  private val channel = new Channel("C123", "slack_test", 1, Some(self.id), Some(false), Some(true), Some(false), None, None, None, None, None, None, None, None, None, None, None)
   private val im = new Im("I123", true, somebodyElse.id, 1, None)
   private val startState = new RtmStartState("http://nothing/", self, team, users = List(self, somebodyElse), channels = List(channel), List.empty, ims = List(im), List.empty)
 
@@ -92,8 +92,8 @@ class ReceptionistTest
 
     "re-interpret messages that were updated" in {
 
-      val previousMessage = new EditMessage(somebodyElse.id, "previous message", currentTimeStamp)
-      val newMessage = new EditMessage(somebodyElse.id, "hello dude4", currentTimeStamp)
+      val previousMessage = new EditMessage(Some(somebodyElse.id), "previous message", currentTimeStamp)
+      val newMessage = new EditMessage(Some(somebodyElse.id), "hello dude4", currentTimeStamp)
 
       sut ! MessageChanged(newMessage, previousMessage, currentTimeStamp, currentTimeStamp, channel.id)
       val result = probe.expectMsgClass(classOf[IncomingMessage])
